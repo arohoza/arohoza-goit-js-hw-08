ee9b163fb59bed03e14d73fccc4ebe495deb6201
arohoza <107406993+arohoza@users.noreply.github.com>
2022-08-25 20:06:28 +0200

    2tasks

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,39 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from 'simplelightbox';
+// Additional styles import
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryRef = document.querySelector('.gallery');
+
+const makeGalleryMarkup = () => {
+  const markup = galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+          <a class="gallery__item" href=${original}>
+            <img href=${original}
+              class="gallery__image"
+              src=${preview}
+              alt="${description}"
+            />
+          </a>
+        `;
+    })
+    .join('');
+  galleryRef.innerHTML = markup;
+};
+
+makeGalleryMarkup(galleryItems);
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'Alt',
+  captionsDelay: 250,
+});
+gallery.on('show.simplelightbox', function () {
+  //   // do something…
+});
+
+console.log(gallery);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,56 @@
+import Player from '@vimeo/player';
 
+var throttle = require('lodash.throttle');
+
+// import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+const onTimeUpdate = function (data) {
+  // data is an object containing properties specific to that event
+  player
+    .getCurrentTime()
+    .then(function (seconds) {
+      // seconds = the current playback position
+
+      //   console.log(seconds);
+      localStorage.setItem('videoplayer-current-time', seconds);
+      //   console.log(localStorage);
+    })
+    .catch(function (error) {
+      // an error occurred
+    });
+};
+
+player.on(
+  'timeupdate',
+  throttle(() => {
+    onTimeUpdate();
+  }, 1000)
+);
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+player
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
